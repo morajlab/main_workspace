e30037b94ead0176c86dd09af12d588466bf8440
morteza-jamali <mortezajamali4241@gmail.com>
2021-03-22 07:52:43 +0430

    dependencies updated

--- tools/packages/popo-core/src/types/index.d.ts
+++ tools/packages/popo-core/src/types/index.d.ts
@@ -1,29 +1,3 @@
-declare module "detect-indent";
-declare module "detect-newline";
-declare module "parse-json";
-declare module "clean-stack";
-declare module "semver";
-declare module "ci-parallel-vars";
-declare module "cmd-shim";
-declare module "read-cmd-shim";
-declare module "pkg-up";
-declare module "typeable-promisify";
-declare module "make-dir";
-declare module "rimraf";
-declare module "p-limit";
-declare module "slash";
-declare module "temp-write";
-declare module "multimatch";
-declare module "cross-spawn";
-declare module "inquirer";
-declare module "path-is-inside";
-declare module "array-includes";
-declare module "project-bin-path";
-declare module "sort-object";
-declare module "chunkd";
-declare module "task-graph-runner";
-declare module "globby";
-
 declare namespace BoltTypes {
   type DependencySet = {
     [key: string]: string;
