afdaa690101070886456e404d5ee4bb6eeaf0d5e
morteza-jamali <mortezajamali4241@gmail.com>
2021-05-08 22:35:09 +0430

    chore: figma-html development configuration updated

---
+++ libs/figma-html/jest.config.js
@@ -0,0 +1,9 @@
+module.exports = {
+  displayName: 'figma-html',
+  preset: '../../jest.preset.js',
+  transform: {
+    '^.+\\.[tj]sx?$': 'babel-jest',
+  },
+  moduleFileExtensions: ['ts', 'tsx', 'js', 'jsx'],
+  coverageDirectory: '../../coverage/libs/figma-html',
+};
