b6b807f9a1d2155b8e0f6a46cc0546f509c06b40
Morteza Jamali <mortezajamali4241@gmail.com>
2021-05-22 02:12:02 +0430

    chore: nx v12 workspace template inited

--- jest.config.js
+++
@@ -1,9 +0,0 @@
-module.exports = {
-  projects: [
-    '<rootDir>/apps/next10',
-    '<rootDir>/libs/react-uwp',
-    '<rootDir>/libs/fluent-app-ui-kit',
-    '<rootDir>/libs/next10/gui',
-    '<rootDir>/apps/desktop',
-  ],
-};
