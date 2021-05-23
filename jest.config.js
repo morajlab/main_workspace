736670580f68e4a89c0b2b61de6bd734af9900ec
Morteza Jamali <mortezajamali4241@gmail.com>
2021-05-23 16:36:05 +0430

    chore: workspace dependencies updated

--- jest.config.js
+++ jest.config.js
@@ -1,12 +1,5 @@
 module.exports = {
   projects: [
-    '<rootDir>/apps/next10',
-    '<rootDir>/libs/react-uwp',
-    '<rootDir>/libs/fluent-app-ui-kit',
-    '<rootDir>/libs/next10/gui',
-    '<rootDir>/apps/desktop',
-    '<rootDir>/libs/morajlab.bit/fluent-app-ui-kit',
-    '<rootDir>/libs/morajlab.bit/fluent-app-ui-kit/ui/commandbar',
     '<rootDir>/libs/morajlab.bit/react-macos-ui',
     '<rootDir>/libs/morajlab.bit/react-macos-ui/ui/button',
     '<rootDir>/libs/morajlab.bit/react-macos-ui/ui/checkbox',
--- libs/morajlab.bit/fluent-app-ui-kit/jest.config.js
+++
@@ -1,9 +0,0 @@
-module.exports = {
-  displayName: 'morajlab.bit-fluent-app-ui-kit',
-  preset: '../../../jest.preset.js',
-  transform: {
-    '^.+\\.[tj]sx?$': 'babel-jest',
-  },
-  moduleFileExtensions: ['ts', 'tsx', 'js', 'jsx'],
-  coverageDirectory: '../../../coverage/libs/morajlab.bit/fluent-app-ui-kit',
-};
