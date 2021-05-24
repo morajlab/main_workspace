8995145be01e98700461dbc049d4e58e11f30ae3
Morteza Jamali <mortezajamali4241@gmail.com>
2021-05-25 02:02:58 +0430

    feat: react-html5-components created

--- jest.config.js
+++ jest.config.js
@@ -2,5 +2,6 @@ module.exports = {
   projects: [
     '<rootDir>/apps/react-html5up-templates/hyperspace/react-html5up-hyperspace',
     '<rootDir>/libs/react-html5up',
+    '<rootDir>/libs/react-html5up/src/components',
   ],
 };
--- libs/react-html5up/src/index.ts
+++ libs/react-html5up/src/index.ts
@@ -1,1 +1,1 @@
-export * from './components/.';
+export * from '@react-html5up-components';
