fdcd1c16220f9f73fc70c2da68a113943ac9db37
Morteza Jamali <mortezajamali4241@gmail.com>
2021-05-29 14:29:18 +0430

    fix: storybook addon-docs configuration fixed

--- .storybook/main.js
+++ .storybook/main.js
@@ -1,9 +1,14 @@
 module.exports = {
   stories: [],
   addons: [
+    {
+      name: '@storybook/addon-docs',
+      options: {
+        configureJSX: true,
+      },
+    },
     '@storybook/addon-actions',
     '@storybook/addon-controls',
-    '@storybook/addon-docs',
     '@storybook/addon-a11y',
     '@storybook/addon-storysource',
     '@storybook/addon-viewport',
