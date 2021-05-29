e89d4db2e31e99880e1cf91cb734c6905a830012
Morteza Jamali <mortezajamali4241@gmail.com>
2021-05-29 09:12:54 +0430

    chore: hyperspace storybook configured

--- .storybook/main.js
+++ .storybook/main.js
@@ -1,7 +1,12 @@
 module.exports = {
   stories: [],
   addons: [
-    '@storybook/addon-knobs/register',
+    '@storybook/addon-actions',
+    '@storybook/addon-controls',
+    '@storybook/addon-docs',
+    '@storybook/addon-a11y',
+    '@storybook/addon-storysource',
+    '@storybook/addon-viewport',
     '@storybook/theming',
     '@storybook/addons',
   ],
--- libs/react-html5up/src/components/src/lib/hyperspace/src/lib/button/src/lib/Button.stories.tsx
+++ libs/react-html5up/src/components/src/lib/hyperspace/src/lib/button/src/lib/Button.stories.tsx
@@ -1,11 +1,44 @@
 import React from 'react';
-import { Button } from './Button';
+import { Story } from '@storybook/react';
+import { Button, IButtonProps } from './Button';
 
 export default {
   component: Button,
-  title: 'Button',
+  title: 'Components/Button',
+  argTypes: {
+    size: {
+      options: ['small', 'large', 'default'],
+      control: {
+        type: 'select',
+      },
+    },
+  },
 };
 
-export const primary = () => {
-  return <Button title="Default Button" />;
+const Template: Story<IButtonProps> = (args) => <Button {...args} />;
+
+export const Default = Template.bind({});
+export const Primary = Template.bind({});
+export const Large = Template.bind({});
+export const Small = Template.bind({});
+
+Default.args = {
+  title: 'Default Button',
+  size: 'default',
+};
+
+Primary.args = {
+  title: 'Primary Button',
+  size: 'default',
+  primary: true,
+};
+
+Large.args = {
+  title: 'Large Button',
+  size: 'large',
+};
+
+Small.args = {
+  title: 'Small Button',
+  size: 'small',
 };
