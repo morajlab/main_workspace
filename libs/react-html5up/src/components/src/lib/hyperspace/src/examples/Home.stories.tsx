bde4bd097d534ea845c9435105bb6ee75742fa5e
Morteza Jamali <mortezajamali4241@gmail.com>
2021-05-30 12:09:56 +0430

    chore: hyperspace home page example updated

--- libs/react-html5up/src/components/src/lib/hyperspace/src/examples/Home.stories.tsx
+++ libs/react-html5up/src/components/src/lib/hyperspace/src/examples/Home.stories.tsx
@@ -5,6 +5,7 @@ import {
   Intro,
   SideNav,
   ISideNavProps,
+  Footer,
 } from '@react-html5up-components/hyperspace';
 
 export default {
@@ -16,6 +17,9 @@ export const Home = () => {
   css.global('html, body', {
     padding: '0 !important',
     margin: '0 !important',
+  });
+  css.global('*, ::before, ::after', {
+    boxSizing: 'inherit',
   });
 
   const SideNavItems: ISideNavProps['navsList'] = [
@@ -34,17 +38,33 @@ export const Home = () => {
     },
   ];
 
+  interface IWrapperStyleProps {}
+
+  const WrapperStyles = ({}: IWrapperStyleProps) => {
+    return css({
+      margin: 0,
+      marginLeft: '18em',
+      padding: 0,
+      border: 0,
+    });
+  };
+
   return (
     <Fragment>
       <SideNav navsList={SideNavItems} />
-      <Intro
-        backgroundImage="https://raw.githubusercontent.com/morteza-jamali/react-html5up-templates/react-html5up-templates-beta/hyperspace/assets/css/images/intro.svg"
-        btnLink="https://github.com/morteza-jamali/react-html5up-templates"
-        btnTitle="Learn More"
-        description="Just another fine responsive site template designed by React HTML5 UP
-        and released for free under the Creative Commons."
-        title="Hyperspace"
-      />
+      <div {...WrapperStyles({})}>
+        <Intro
+          backgroundImage="https://raw.githubusercontent.com/morteza-jamali/react-html5up-templates/react-html5up-templates-beta/hyperspace/assets/css/images/intro.svg"
+          btnLink="https://github.com/morteza-jamali/react-html5up-templates"
+          btnTitle="Learn More"
+          description="Just another fine responsive site template designed by React HTML5 UP
+        and released for free under the Creative Commons.."
+          title="Hyperspace"
+        />
+        <Footer
+          list={['© Untitled. All rights reserved', 'Design: HTML5 UP']}
+        />
+      </div>
     </Fragment>
   );
 };
